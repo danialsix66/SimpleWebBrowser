f95c3a1a98f11f7a0a68a4160c12bc316abc3381
Danial <132660693+danialsix66@users.noreply.github.com>
2024-09-16 09:51:48 +0330

    Toty WebBrowser Version2
    Signed-off-by: Danial <132660693+danialsix66@users.noreply.github.com>

--- Browser/src/SimpleBrowser.java
+++ Browser/src/SimpleBrowser.java
@@ -9,37 +9,61 @@ public class SimpleBrowser extends JFrame {
     private JTextField urlField;
 
     public SimpleBrowser() {
-        setTitle("Simple Browser");
+        setTitle("Toty WebBrowser");
+
+        // تنظیم آیکون برای فریم اصلی
+        Image icon = Toolkit.getDefaultToolkit().getImage("src\\toty.png");
+        setIconImage(icon);
+
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(Color.DARK_GRAY);
 
-        // ساخت پنل برای نوار آدرس
         JPanel panel = new JPanel();
-        urlField = new JTextField(30);
-        JButton goButton = new JButton("Go");
+        panel.setBackground(Color.DARK_GRAY);
+        urlField = new JTextField(50);
+        JButton goButton = new JButton("Search");
+        JButton ABButton = new JButton("About Me");
 
         panel.add(urlField);
         panel.add(goButton);
+        panel.add(ABButton);
         add(panel, BorderLayout.NORTH);
 
-        // ساخت JEditorPane برای نمایش صفحات وب
         editorPane = new JEditorPane();
         editorPane.setEditable(false);
         JScrollPane scrollPane = new JScrollPane(editorPane);
+        scrollPane.setBackground(Color.DARK_GRAY);
         add(scrollPane, BorderLayout.CENTER);
 
-        // گوش دادن به دکمه
         goButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 loadPage(urlField.getText());
             }
         });
+
+        // اصلاح شده
+        ABButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(SimpleBrowser.this, "Hi im danial and this app Developed By Me Workwith this app and Enjoy It");
+            }
+        });
     }
 
     private void loadPage(String url) {
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            if (url.startsWith("www.")) {
+                url = "http://" + url;
+            } else {
+                url = "http://www." + url;
+            }
+        }
+
         try {
             editorPane.setPage(new URL(url));
+            editorPane.setBackground(Color.DARK_GRAY);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(this, "Error loading page: " + e.getMessage());
         }
